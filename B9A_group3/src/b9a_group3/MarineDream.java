5570e91d893efdc3667caeecc7e51c8794a25d87
Lamar_Aljoufi <ghsq2001@gmail.com>
2022-11-04 13:29:34 +0300

    creating classes

---
+++ B9A_group3/src/b9a_group3/MarineDream.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b9a_group3;
+
+/**
+ *
+ * @author DELL
+ */
+public class MarineDream {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
