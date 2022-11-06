37bb1dd5673a9526eeea336d5aed01936c90317d
Farah_Alamayrah <farahalamayrah@gmail.com>
2022-11-06 11:19:09 -0800

    farah comit

--- B9A_group3/src/b9a_group3/Ticket.java
+++ B9A_group3/src/b9a_group3/Ticket.java
@@ -1,14 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package b9a_group3;
 
-/**
- *
- * @author DELL
- */
 public class Ticket {
+
+
+    private int ticketID;
+    private Customer customer;
+    private Fishing fishing;
+    private Sailing sailing;
+
+    public Ticket(Customer customer, Fishing fishing) {
+        this.ticketID = (int) ((Math.random() * (1000 - 100)) + 100);
+        this.customer = customer;
+        this.fishing = fishing;
+    }
+
+    public Ticket(Customer customer, Sailing sailing) {
+        this.ticketID = (int) ((Math.random() * (1000 - 100)) + 100);
+        this.customer = customer;
+        this.sailing = sailing;
+    }
+
+    public int getTicketID() {
+        return ticketID;
+    }
+
+    public void setTicketID(int ticketID) {
+        this.ticketID = ticketID;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Service getBooking() {
+        if (fishing != null) {
+            return fishing;
+        } else {
+            return sailing;
+        }
+    }
+
+    public void setBooking(Service service) {
+        if (service instanceof Fishing) {
+            this.fishing = (Fishing) service;
+        } else {
+            this.sailing = (Sailing) service;
+        }
+    }
+
     
+    public Fishing getFishing() {
+        return fishing;
+    }
+
+    public Sailing getSailing() {
+        return sailing;
+    }
+    
+    
+    public String displayTicket() {
+
+        if (fishing != null) {
+            return "\n----------- Thank you for your reserveation from Marine Dream -----------"
+                    + "\nCustomer details: "
+                    + "\n-- Name: " + customer.getName() + "\t Phone: " + customer.getPhone() + "\tAge: " + customer.getAge()
+                    + "\nTicket details:"
+                    + "\n-- Service type: Fishing - Basic, ID: " + getTicketID() + "\tTime: " + fishing.getTime() + "\tDate: " + fishing.getDate() + "\tDuration: " + fishing.getDuration()
+                    + "\n-- Fishing Payload: " + fishing.getFishingPayLoad() + "\tequipments: basic "
+                    + "\n"
+                    + "\nTicket price: " + fishing.getPrice() + "$"
+                    + "\n----------------------------------------------";
+        } else {
+
+            return "\n----------- Thank you for your reserveation from Marine Dream -----------"
+                    + "\nCustomer details: "
+                    + "\n-- Name: " + customer.getName() + "\t Phone: " + customer.getPhone() + "\tAge: " + customer.getAge()
+                    + "\nTicket details:"
+                    + "\n-- Service type: Sailing - Basic, ID: " + getTicketID() + "\tTime: " + sailing.getTime() + "\tDate: " + sailing.getDate() + "\tDuration: " + sailing.getDuration()
+                    + "\tNumber of people: " + sailing.getNumOfPeople()
+                    + "\nTicket price " + sailing.getPrice() + "$"
+                    + "\n--------------------------------------------------";
+        }
+    }
 }
---
+++ random/src/random/Random.java
@@ -0,0 +1,22 @@
+
+package random;
+
+/**
+ *
+ * @author DELL
+ */
+public class Random {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Enter Traces file name?"); 
+        System.out.println("Traces.txt");
+        System.out.println("Done reading");
+        System.out.println("Output results are stored in file outputResults.txt");
+    
+    }
+    
+}
