be69b3a49d26745c7338498c70316d3a73dd0e76
Kefayaaltaezi <112615292+Kefayaaltaezi@users.noreply.github.com>
2022-11-06 12:36:20 +0300

    Create TicketTest.java
    committing TicketTest  Class

---
+++ B9A_group3/test/B9A_group3/TicketTest.java
@@ -0,0 +1,61 @@
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kefayaaltaezi
+ */
+public class TicketTest {
+    
+    public TicketTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of setBooking method, of class Ticket.
+     */
+    @Test
+    public void testSetFishing() {
+        System.out.println("setBooking");
+        Fishing fishing = new Fishing ("9","10/2",4);
+        Customer customer = new Customer ("Kofa","0532098887",21);
+        Ticket instance = new Ticket (customer,fishing);
+        assertNotNull(instance.getFishing());
+       
+        
+    }
+
+    @Test
+    public void testSetSailing() {
+        System.out.println("setBooking");
+        Sailing sailing = new Sailing (5,"9","10/2",4);
+        Customer customer = new Customer ("Kofa","0532098887",21);
+        Ticket instance = new Ticket (customer,sailing);
+        assertNotNull(instance.getSailing());
+       
+        
+    }
+    
+    
+    
+}
