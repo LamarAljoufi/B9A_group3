2d7164064ad12feffaf3794f6a58b2c5cff4bcc9
Noura Alqarni <96919881+nouralqarni@users.noreply.github.com>
2022-11-06 12:24:28 +0300

    Update MarineDreamDemo.java

--- B9A_group3/src/b9a_group3/MarineDreamDemo.java
+++ B9A_group3/src/b9a_group3/MarineDreamDemo.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package b9a_group3;
 
-/**
- *
- * @author DELL
- */
 public class MarineDreamDemo {
 
     /**
@@ -31,5 +22,11 @@ public class MarineDreamDemo {
 
         return new Customer(name, phone, age);
     }
-    
+    static void displayUgrade(Service service, int choice) {
+        if (service instanceof Fishing) {
+            output.println(((Fishing) service).upgradeTicket(choice));
+        } else {
+            output.println(((Sailing) service).upgradeTicket(choice));
+        }
+    }
 }
