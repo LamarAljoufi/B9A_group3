0a58ee024a8363775832fd01bcbd4b5bb94dee7a
Kefayaaltaezi <112615292+Kefayaaltaezi@users.noreply.github.com>
2022-11-06 12:39:25 +0300

    Update MarineDreamDemo.java
    last commit: committing the main class

--- B9A_group3/src/b9a_group3/MarineDreamDemo.java
+++ B9A_group3/src/b9a_group3/MarineDreamDemo.java
@@ -1,13 +1,70 @@
 package b9a_group3;
 
 public class MarineDreamDemo {
+    
+    private static Scanner input;
+    private static PrintWriter output;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException {
+        input = new Scanner(System.in);
+        output = new PrintWriter("output.txt");
+        ArrayList<Ticket> tickets = new ArrayList<>();
+
+        displayMenu();
+        int choice = input.nextInt();
+
+        //if users chooses Fishing (fill info)
+        if (choice == 1) {
+            System.out.print("\n------- Redervation Details -------");
+            System.out.print("\n-- Enter time in hours: ");
+            String time = input.next();
+
+            System.out.print("-- Enter date as dd/mm: ");
+            String date = input.next();
+
+            System.out.print("-- Enter duration in hours: ");
+            int duration = input.nextInt();
+
+            Fishing fishing = new Fishing(time, date, duration);
+
+            tickets.add(new Ticket(createCustomer(), fishing));
+
+        } else if (choice == 2) {
+            System.out.print("\n------- Redervation Details -------");
+            System.out.print("\n-- Enter time: ");
+            String time = input.next();
+
+            System.out.print("-- Enter date as dd/mm: ");
+            String date = input.next();
+
+            System.out.print("-- Enter duration in hours: ");
+            int duration = input.nextInt();
+
+            System.out.print("-- Enter number of people: ");
+            int numOfPeople = input.nextInt();
+            Sailing sailing = new Sailing(numOfPeople, time, date, duration);
+
+            tickets.add(new Ticket(createCustomer(), sailing));
+
+        }
+
+        output.print(tickets.get(0).displayTicket()); // index
+        System.out.println("\n----------- You booked your ticket successfully -----------\n");
+
+        upgradeMenu(tickets.get(0).getBooking());
+        choice = input.nextInt();
+
+        if (tickets.get(0).getBooking().checkUpgradeTicket(choice)) {
+            displayUgrade(tickets.get(0).getBooking(), choice);
+        }
+
+        System.out.println("\nThanks for your reservation, have fun in your trip  ");
+
+        output.close();
+        output.flush();
+
     }
+
       //crearting customer object (fill info)
     static Customer createCustomer() {
         System.out.print("\n------- Customer Information -------");
