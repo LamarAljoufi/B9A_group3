8ee52e1ce8811379dc701f23bda51688677a149f
Kefayaaltaezi <112615292+Kefayaaltaezi@users.noreply.github.com>
2022-11-06 12:16:45 +0300

    Update Fishing.java
    committing fishing class

--- B9A_group3/src/b9a_group3/Fishing.java
+++ B9A_group3/src/b9a_group3/Fishing.java
@@ -5,10 +5,45 @@
  */
 package b9a_group3;
 
-/**
- *
- * @author DELL
- */
-public class Fishing {
-    
+public class Fishing extends Service {
+
+    private int fishingPayLoad;
+
+
+    public Fishing(String time, String date, int duration) {
+        super(time, date, duration);
+        super.setPrice(2000);
+        this.fishingPayLoad = 50;
+
+    }
+
+    public int getFishingPayLoad() {
+        return fishingPayLoad;
+    }
+
+    public void setFishingPayLoad(int fishingPayLoad) {
+        this.fishingPayLoad = fishingPayLoad;
+    }
+
+    public String upgradeTicket(int upgrade) {
+        if (upgrade == 1) {
+            super.setPrice(super.getPrice() + 1500);
+            return "\n----------- Ticket is upgrader to VIP -----------"
+                    + "\nVIP services:"
+                    + "\n-- Fisherman helper"
+                    + "\n-- 2x Fishing payload"
+                    + "\n-- Inshore fishing"
+                    + "\nUpgraded ticket price: " + super.getPrice() + "$"
+                    + "\n\"----------------------------------------------";
+        } else {
+            super.setPrice(super.getPrice() + 3000);
+            return "\n----------- Ticket is upgrader to VVIP -----------"
+                    + "\nVVIP services:"
+                    + "\n-- Fisherman helper"
+                    + "\n-- 3x Fishing payload"
+                    + "\n-- Deep sea fishing"
+                    + "\nUpgraded ticket price: " + super.getPrice() + "$"
+                    + "\n\"----------------------------------------------";
+        }
+    }
 }
